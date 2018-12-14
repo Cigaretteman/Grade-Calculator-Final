ac6eeede381c155dab46d65240349e005b248b60
EuphoricHangover <Fl0yd@dis.cool>
2018-12-14 14:13:41 -0500

    .

--- Grade Calculator Final/WebForm1.aspx.designer.cs
+++ Grade Calculator Final/WebForm1.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Grade_Calculator_Final {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// CourseName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CourseName;
+        
+        /// <summary>
         /// UpdatePanel2 control.
         /// </summary>
         /// <remarks>
@@ -65,6 +74,15 @@ namespace Grade_Calculator_Final {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TableHeaderRow headerRow;
+        
+        /// <summary>
+        /// lblGpaOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGpaOut;
         
         /// <summary>
         /// SqlDataSource1 control.
